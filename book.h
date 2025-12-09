966d2d95bb25dde14eb5a879236cec1a8e11e7df
Leny Chang <141756738+lenychang520@users.noreply.github.com>
2025-12-09 18:48:37 +0800

    Add Save function to book.h
    Added Save function to save contact list.

--- book.h
+++ book.h
@@ -5,16 +5,14 @@
 #include <stdlib.h>
 #include <errno.h>
 
-typedef struct Profile
-{
+typedef struct Profile {
 	char name[20];
 	char sex[10];
 	char phone[12];
 	int age;//这里的age不要改成数组的形式，否则后续打印的时候会出bug
 }Profile;
 
-typedef struct Contact
-{
+typedef struct Contact {
 	Profile* date;
 	int count;
 	int capacity;
@@ -25,9 +23,10 @@ void Add(Contact* con);
 void Initial(Contact* con);
 void Display(const Contact* con);
 void Del(Contact* con);
-int find(Contact* con, char name[20]);
+int find(const Contact* con, char name[20]);
 void Find(const Contact* con);
 void Modify(Contact* con);
 void Sort(Contact* con);
 int sort(const void* name1, const void* name2);
-void Modify_capacity(Contact* con);
+void Modify_capacity(Contact* con);//扩容
+void Save(const Contact* con);//保存通讯录
